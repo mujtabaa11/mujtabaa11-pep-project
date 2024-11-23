fa470be738b469150bb2c59efadddfd3080c0d6f
Mohamed Mujtaba <Mohamed.Mugtaba11@gmail.com>
2024-11-23 23:53:29 +0000

    AccountService changed

--- src/main/java/Service/AccountService.java
+++ src/main/java/Service/AccountService.java
@@ -1,5 +1,7 @@
 package Service;
 
+import Model.Account;
+
 public class AccountService {
     public AccountDAO accountDAO;
 
@@ -9,6 +11,15 @@ public class AccountService {
 
     public AccountService(AccountDAO accountDAO) {
         this.accountDAO = accountDAO;
+    }
+
+    public Account registerAccount(Account account) {
+        if (account == null || account.getUsername() == null || account.getUsername().trim().isEmpty() || account.getPassword().length() < 4) {
+            System.out.println("Invalid account: name cannot be null and password must be at least four characters long");
+            return null;
+        }
+        
+        return accountDAO.insertAccount(account);
     }
     
 }
